19b2166099143a2b52038f968ee76454d4cf5abd
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2022-10-15 16:33:01 +0400

    first steps

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = Flask(__name__)
+# @app.route('/albums/1')
+# def hello_world():
+#     return 'Hello, Flask'
+#
+#
+# @app.route('/albums/<int:album_number>')
+# def albums(album_number):
+#     return 'The {} album.'.format(album_number)
+#
+#
+# @app.route('/albums/<int:album_number>/<song_number>')
+# def album(album_number, song_number):
+#     return 'The {} album and {} musician performer.'.format(album_number, song_number)
+
+# @app.route('/')
+# def calc():
+#     return render_template('index.html')
+
+# @app.route('/<int:numb>/')
+# def index(numb):
+#     return render_template('index.html', number = numb*2, text = 'Ваше число {}, умноженное на 2: {}'.format(numb, numb*2))
+
+template = '''
+    {%if symbol == '+'%}
+        {{first+second}}
+    {%elif symbol == '-'%}
+        {{first-second}}
+    {%elif symbol == '**'%}
+        {{first**second}}
+    {%elif symbol ==':'%}
+        {% if second != 0.0%}
+            {{first/second}}
+        {% else %}
+            {{Ошибка}}
+        {%endif%}
+    {%elif symbol == '*'%}
+        {{first*second}}
+    {%else%}
+        {{Ошибка}}
+    {%endif%}
+'''
+@app.route('/<float:first>/<string:symbol>/<float:second>/')
+def index(first, symbol, second):
+    return render_template('index.html',  first=first, second=second, symbol=symbol)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True, host='127.0.0.1', port=5000)
